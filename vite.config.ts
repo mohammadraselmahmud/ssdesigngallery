418cdc0cb3641b91353b03994377cca893c9df76
MD Nazmul Hasan <105441794+mdnazmulhasanniloy@users.noreply.github.com>
2026-02-10 12:34:57 +0600

    add icons

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,13 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+     preview: {
+  allowedHosts: [
+    "ssdesigngallery.com",
+    "www.ssdesigngallery.com",
+    "api.ssdesigngallery.com",
+  ],
+},
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
